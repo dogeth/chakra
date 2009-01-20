acfa3b7da1ff1cf5cc5fc023f376fd9c570bb181
Kevin Read <readkev@gmail.com>
2009-01-20 16:23:12 +1000

    Added chakra and goddess tables

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :goddesses
+
+  map.resources :chakras
+
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
