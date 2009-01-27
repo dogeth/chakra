00af1298879f924b7ab08a8959c31a476bb1ce5d
Kevin Read <readkev@gmail.com>
2009-01-27 13:22:43 +1000

    Added images cleanup of css/script

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -8,23 +8,16 @@ $(function() {
   	var shapes= $(".shape")
   	var xoffset = new Array();
   	var yoffset = new Array();
-  	var maxWidth = 0; var maxHeight = 0;
+  	var maxImageWidth = 0; var maxImageHeight = 0;
   	for (var j = 0; j < shapes.length ; j++) {
     	var w = parseInt($(shapes[j]).width()); var h = parseInt($(shapes[j]).height());
-    	if (w > maxWidth) {maxWidth = w}; if (h > maxHeight) {maxHeight = h};
+    	if (w > maxImageWidth) {maxImageWidth = w}; if (h > maxImageHeight) {maxImageHeight = h};
     	xoffset[j] = Math.floor(w / 2); yoffset[j] = Math.floor(h / 2);
   	};
-  	var maxImageLength = (maxWidth > maxHeight) ? maxWidth : maxHeight;
+  	var maxImageLength = (maxImageWidth > maxImageHeight) ? maxImageWidth : maxImageHeight;
 
   	var canvasWidth = parseInt($("#canvas").width()); var canvasHeight = parseInt($("#canvas").height());
   	var maxCanvasLength = (canvasWidth > canvasHeight) ? canvasWidth : canvasHeight;
-
-    $("#debug").append("maxWidth" + maxWidth);
-    $("#debug").append("<br />");
-    $("#debug").append("maxHeight" + maxHeight);
-    $("#debug").append("<br />");
-    $("#debug").append("maxImageLength" + maxImageLength);
-    $("#debug").append("<br />");
 
   	var radius = maxCanvasLength / 2 - Math.floor(maxImageLength / 2); 
   	var steps = 140;
@@ -37,18 +30,12 @@ $(function() {
       xValues[i] = Math.floor(centerX + radius * Math.cos(2 * Math.PI * i / steps)) ;
       yValues[i] = Math.floor(centerY + radius * Math.sin(2 * Math.PI * i / steps)) ;
   	};
-  	//for (var i = 0; i < steps; i++) {
-  		//    $("#debug").append("Pos" + i + ":" + xValues[i] + "," + yValues[i] + " ");
- 		// }
-		//  $("#debug").append("<br />");
 
   	var position = new Array();
   	for (var j = 0; j < shapes.length ; j++) {
     	position[j] = Math.floor((steps * j) / shapes.length);
     	x = (xValues[position[j]] - xoffset[j]) ; y = (yValues[position[j]] - yoffset[j]);
-    	$(shapes[j]).css({left: x + "px", top: y + "px"}).show();	
-		//    $("#debug").append("shape" + j + " pos:" + position[j] + " " + xValues[position[j]] + "px," + yValues[position[j]] + "px ");
-		//    $("#debug").append("<br />");
+    	$(shapes[j]).css({left: x + "px", top: y + "px"}); //kkk .show();	
   	};
 
    	function rotate_shapes() {
