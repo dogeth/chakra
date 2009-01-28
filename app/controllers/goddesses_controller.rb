fa47ce5b50fe40f73a7870485379f60a61999f95
Kevin Read <readkev@gmail.com>
2009-01-28 10:14:00 +1000

    Added chakra symbol to the background of chakra/goddess pages

--- app/controllers/goddesses_controller.rb
+++ app/controllers/goddesses_controller.rb
@@ -1,7 +1,7 @@
 class GoddessesController < ApplicationController
   caches_page :show
   def show
-	@goddess = Goddess.find_by_name(params[:goddess])
+	@goddess = Goddess.find_by_name(params[:goddess], :include => :chakra)
     unless @goddess 
 		logger.error("Attempt to access invalid goddess: '#{params[:goddess]}'") 
 	    redirect_to :controller => 'chakras', :action => 'index'
