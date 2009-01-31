c5500b72436bf5dd3605710d0680dd18882e115e
lachlan <lachlan.dowding@gmail.com>
2009-01-31 13:32:09 +1000

    Fixed indenting in GodessesController#show

--- app/controllers/goddesses_controller.rb
+++ app/controllers/goddesses_controller.rb
@@ -1,11 +1,12 @@
 class GoddessesController < ApplicationController
   caches_page :show
+  
   def show
-	@goddess = Goddess.find_by_name(params[:goddess], :include => :chakra)
+	  @goddess = Goddess.find_by_name(params[:goddess], :include => :chakra)
     unless @goddess 
-		logger.error("Attempt to access invalid goddess: '#{params[:goddess]}'") 
+		  logger.error("Attempt to access invalid goddess: '#{params[:goddess]}'") 
 	    redirect_to :controller => 'chakras', :action => 'index'
-    end 
+    end
     render :layout => 'chakras'
   end
 
